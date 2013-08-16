62a4930c5150dfd8029a17e51050f215a047f366
Steve Kondik <shade@chemlab.org>
2013-08-16 14:29:44 -0700

    core: Add some static libraries
     * Add static versions of libdiskconfig and libsysutils for MiniVold
    
    Change-Id: I9ea9498dcff3f094bbcbb1e05e6d10bc9b83ac74
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- libsysutils/Android.mk
+++ libsysutils/Android.mk
@@ -2,9 +2,7 @@ ifneq ($(BUILD_TINY_ANDROID),true)
 
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:=                             \
+common_src_files :=                           \
                   src/SocketListener.cpp      \
                   src/FrameworkListener.cpp   \
                   src/NetlinkListener.cpp     \
@@ -14,6 +12,9 @@ LOCAL_SRC_FILES:=                             \
                   src/ServiceManager.cpp      \
                   EventLogTags.logtags
 
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= $(common_src_files)
 LOCAL_MODULE:= libsysutils
 
 LOCAL_C_INCLUDES :=
@@ -23,5 +24,12 @@ LOCAL_CFLAGS := -Werror
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= $(common_src_files)
+LOCAL_MODULE:= libsysutils
+LOCAL_C_INCLUDES := $(KERNEL_HEADERS)
+LOCAL_CFLAGS :=
+include $(BUILD_STATIC_LIBRARY)
 
 endif
