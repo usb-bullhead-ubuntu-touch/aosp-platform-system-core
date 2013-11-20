e67c45a1c5400cd0dac387828219199b15448545
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2013-11-20 00:15:03 -0200

    init.c: only skip setting up /dev if we're not in recovery mode
    Change-Id: I6490c506a6887c4ef49477f974b408e6d004fad0
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    Signed-off-by: Rex Tsai (蔡志展) <rex.tsai@canonical.com>

--- init/init.cpp
+++ init/init.cpp
@@ -991,6 +991,7 @@ static void selinux_initialize(bool in_kernel_domain) {
 }
 
 int main(int argc, char** argv) {
+    struct stat s;
     if (!strcmp(basename(argv[0]), "ueventd")) {
         return ueventd_main(argc, argv);
     }
@@ -1009,10 +1010,13 @@ int main(int argc, char** argv) {
     // Get the basic filesystem setup we need put together in the initramdisk
     // on / and then we'll let the rc file figure out the rest.
     if (is_first_stage) {
-        mount("tmpfs", "/dev", "tmpfs", MS_NOSUID, "mode=0755");
-        mkdir("/dev/pts", 0755);
         mkdir("/dev/socket", 0755);
-        mount("devpts", "/dev/pts", "devpts", 0, NULL);
+        if (stat("/sbin/recovery", &s) == 0) {
+            /* Only create and mount everything if we're in recovery mode */
+            mount("tmpfs", "/dev", "tmpfs", MS_NOSUID, "mode=0755");
+            mkdir("/dev/pts", 0755);
+            mount("devpts", "/dev/pts", "devpts", 0, NULL);
+        }
         mount("proc", "/proc", "proc", 0, NULL);
         mount("sysfs", "/sys", "sysfs", 0, NULL);
     }
