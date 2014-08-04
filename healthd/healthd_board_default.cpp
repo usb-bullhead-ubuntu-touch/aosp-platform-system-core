eae38aeb8aa28b4447aef7f4f0cc93053a141702
Ricardo Salveti de Araujo <rsalveti@rsalveti.net>
2014-08-04 11:56:48 -0300

    healthd: don't print to the kernel log on battery updates
    Change-Id: Ia6b791d8dd5bbee8ba52dc42d864533354169cd6
    Signed-off-by: Ricardo Salveti de Araujo <rsalveti@rsalveti.net>
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- healthd/healthd_board_default.cpp
+++ healthd/healthd_board_default.cpp
@@ -25,5 +25,6 @@ void healthd_board_init(struct healthd_config*)
 int healthd_board_battery_update(struct android::BatteryProperties*)
 {
     // return 0 to log periodic polled battery status to kernel log
-    return 0;
+    /* On Ubuntu we don't want this to be constantly updating the kernel log */
+    return 1;
 }
