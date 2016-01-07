0b044a2afbd8de92195bda8ed9d06c4e910c892f
Ondrej Kubik <ondrej.kubik@canonical.com>
2016-01-07 14:35:56 +0000

    make_ext4fs: making file permission algorithm more robust
    - file permissions for Ubuntu overlay in Android image are now handled correctly
    - minor fix for file permissions in Ubuntu image when object is not found in content file
    
    Change-Id: I8a2bea2cbabf6519af9b9324974befbea1d47205
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -213,6 +213,7 @@ static const struct fs_path_config android_dirs[] = {
     { 00755, AID_ROOT,   AID_ROOT,   0, "system/etc/ppp" },
     { 00755, AID_ROOT,   AID_SHELL,  0, "vendor" },
     { 00777, AID_ROOT,   AID_ROOT,   0, "sdcard" },
+    { 00755, AID_ROOT,   AID_ROOT,   0, "system/ubuntu*" },
     { 00755, AID_ROOT,   AID_ROOT,   0, 0 },
 };
 
@@ -266,6 +267,9 @@ static const struct fs_path_config android_files[] = {
     { 00750, AID_ROOT,      AID_SHELL,     0, "init*" },
     { 00750, AID_ROOT,      AID_SHELL,     0, "sbin/fs_mgr" },
     { 00640, AID_ROOT,      AID_SHELL,     0, "fstab.*" },
+    { 00644, AID_ROOT,      AID_ROOT,      0, "system/ubuntu/usr/etc/*" },
+    { 00755, AID_ROOT,      AID_ROOT,      0, "system/ubuntu/usr/bin/*" },
+    { 00644, AID_ROOT,      AID_ROOT,      0, "system/ubuntu/usr/lib/*" },
     { 00644, AID_ROOT,      AID_ROOT,      0, 0 },
 };
 
@@ -297,7 +301,12 @@ static inline void fs_config(const char *path, int dir,
     }
     *uid = pc->uid;
     *gid = pc->gid;
-    *mode = (*mode & (~07777)) | pc->mode;
+    if(!strncmp("system/ubuntu/", path, 14)) {
+        // this is ubuntu overlay, merge with existing host os file permissions
+        *mode = (*mode & (~07000)) | pc->mode;
+    } else {
+        *mode = (*mode & (~07777)) | pc->mode;
+    }
     *capabilities = pc->capabilities;
 
 #if 0
